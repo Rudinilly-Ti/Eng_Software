31e48b51dc5377af2e6ef93d99d7a0577efb91a8
Inhegaz <vinipegacion@gmail.com>
2022-08-23 10:10:21 -0300

    Component Button Abrir Loja

---
+++ src/components/OpenPizzaria/index.js
@@ -0,0 +1,16 @@
+import './style.css'
+
+const OpenPizzaria = () => {
+    return (
+        <div class="buttonSetOn">
+            <div class="open">
+                <p class="paragraph"> Abrir loja </p>
+                <div id="retangle">
+                    <button class="ball"> </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default OpenPizzaria;
