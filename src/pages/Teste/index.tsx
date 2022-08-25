9daae693ffad9a2c1624d91e6fefedeb999bedbb
Rudinilly Rodrigues <rudinilly-TI@outlook.com>
2022-08-24 22:06:48 -0300

    new SideBar #WEB-006 [feature] (#2)
    * new SideBar #WEB-006 [feature]
    
    * new NavBar #WEB-006 [feature]
    
    * height cover div #WEB-006 [fix]
    
    Co-authored-by: Rudinilly-ti <rudinilly-ti@outolook.com>

--- src/pages/Teste/index.tsx
+++ src/pages/Teste/index.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import ProductCard from '../../components/ProductCard';
 import './styles.scss';
-
-const pizza = {
-  id: 1,
-  name: 'Pizza de Frango com Catupiry M',
-  category: { id: 1, name: 'Pizza' },
-  description: 'Frango, queijo, presunto, catupiry',
-  available: true,
-  price: 28.99,
-  img_url:
-    'https://www.receiteria.com.br/wp-content/uploads/pizza-de-frango-de-liquidificador-00.jpg',
-};
+import SideBar from '../../components/SideBar';
+import NavBar from '../../components/NavBar';
 
 const Teste: React.FC = () => {
-  return <ProductCard product={pizza} />;
+  return (
+    <div className="container">
+      <NavBar />
+    </div>
+  );
 };
 
 export default Teste;
