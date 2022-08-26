2d64e714e6695d4e6210ff7cd4f9b6b5dcf560b5
Junior Cunha <junior@Juniors-MacBook-Air.local>
2022-08-26 19:55:56 -0300

    adding new generic modal #WEB-015 [feature]

---
+++ src/components/GenericModal/index.tsx
@@ -0,0 +1,33 @@
+import React, { MouseEventHandler, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+import './styles.scss';
+
+type Props = {
+  title: string;
+  children: JSX.Element;
+  show: boolean;
+  onClose(): void;
+};
+
+const ProductCard = ({ title, children, show, onClose }: Props) => {
+  return (
+    <div className={`modal-bg ${show ? 'modal-on' : 'modal-off'}`}>
+      <div className="modal">
+        <div className="close-modal-button">
+          <FontAwesomeIcon
+            onClick={onClose}
+            className="close-icon"
+            icon={solid('xmark')}
+          />
+        </div>
+        <div className="modal-title">
+          <h4>{title}</h4>
+        </div>
+        <div className="modal-content">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
