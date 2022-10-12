e0938f5609cbea429e128c3f814769a0c5844531
Junior Cunha <54959969+junior-ch-rc@users.noreply.github.com>
2022-10-12 15:34:48 -0300

    Revert "Web 014 (#7)"
    This reverts commit b0fbdded2bbb30c9b62e04df4f02fbe63faee742.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MyRoutes from './routes';
-import './styles.scss';
 
 const App: React.FC = () => (
   <BrowserRouter>
