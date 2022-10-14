5eaa49f64d2f5db7d0d61380b2458e736c9a7f05
Junior Cunha <54959969+junior-ch-rc@users.noreply.github.com>
2022-10-14 19:19:44 -0300

    Inserindo funcionalidades do gerenciamento de produtos (#8)
    * listar e cadastrar parcial de produtos #WEB-020 [feature]
    
    * iniciando funcao de editar produto #WEB-020 [feature]
    
    * finalizando gerenciamento de produtos #WEB-020 [feature]
    
    Co-authored-by: Junior Cunha <junior@Juniors-MacBook-Air.local>

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -4,22 +4,41 @@ import './styles.scss';
 
 type Props = {
   product: Product;
+  className?: string;
+  click?(): void;
 };
 
-const ProductCard = ({ product }: Props) => {
+const ProductCard = ({ product, className, click }: Props) => {
   return (
-    <div className="product-card">
-      <img className="product-image" src={product.img_url} alt={product.name} />
+    <div
+      className={`product-card ${className}`}
+      onClick={click}
+      onKeyDown={click}
+      tabIndex={0}
+      role="link"
+    >
+      <img
+        className="product-image"
+        src={product.imageUrl}
+        alt={product.name}
+      />
       <div className="product-details">
         <div className="product-details-bg" />
         <div className="product-details-info">
           <h5 className="name">{product.name}</h5>
           <p className="description">{product.description}</p>
-          <h5 className="price">{product.price}</h5>
+          <h5 className="price">
+            {product.price ? `R$ ${product.price / 100}` : ''}
+          </h5>
         </div>
       </div>
     </div>
   );
+};
+
+ProductCard.defaultProps = {
+  className: '',
+  click: null,
 };
 
 export default ProductCard;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from '../components/Layout';
 import Teste from '../pages/Teste';
 import Tracking from '../pages/Tracking';
+import ProductManagement from '../pages/ProductManagement';
+
+const user = true;
 
 const MyRoutes: React.FC = () => (
   <Routes>
-    <Route path="/" element={<Layout user={false} />}>
-      <Route path="products" element={<Teste />} />
+    <Route path="/" element={<Layout user={user} />}>
+      <Route
+        path="products"
+        element={user ? <ProductManagement /> : <Teste />}
+      />
       <Route path="tracking" element={<Tracking />} />
     </Route>
   </Routes>
